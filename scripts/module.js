ed8d8f370f8da01b28b5b360cbad1adc2a3b06a7
Bodie Sullivan <bodiesullivan@gmail.com>
2021-04-26 20:57:49 -0500

    Initial commit

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,36 @@
-Hooks.once('init', async function() {
 
+
+export class SR5EnhancedActorSheet extends SR5ACtorSheet {
+
+    constructor(...args) {
+        super(...args);
+
+    }
+
+    get template() {
+        const templatePath = 'modules/SR5EnhancedSheets/templates/character.html';
+        return templatePath;
+    }
+
+    getData() {
+        const data = (super.getData());
+        return data;
+    }
+}
+
+
+// Register Tidy5e Sheet and make default character sheet
+Actors.registerSheet("SR5EnhancedActorSheet", SR5EnhancedActorSheet, {
+	types: ["character"],
+	makeDefault: true
+});
+
+
+
+Hooks.once('init', async function() {
+    console.log('init happened');
 });
 
 Hooks.once('ready', async function() {
-
+    console.log('ready happened');
 });
